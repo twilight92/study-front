084eef1db97f2316a6390f488add442d7d45a8a1
tnwjd2050 <tnwjd2050@naver.com>
2020-07-26 22:33:22 +0900

    따옴표 제거

--- week4/study/app.js
+++ week4/study/app.js
@@ -21,4 +21,4 @@ const numbers = [0, -50, 444, 1, 23, 14, 5, 6, 234, 99];
 
 console.log(getMaxOrMin("max", numbers)) // 444
 console.log(getMaxOrMin("min", numbers)) // -50
-console.log(getMaxOrMin("111", numbers)) // "올바른 타입이 아닙니다."
+console.log(getMaxOrMin(111, numbers)) // "올바른 타입이 아닙니다."
