f27ce73db4d95bf8a9093b56e6c28795cd8bf156
tnwjd2050 <tnwjd2050@naver.com>
2020-07-30 22:56:36 +0900

    JS에서의 가라 enum 활용

--- week4/study/app.js
+++ week4/study/app.js
@@ -1,11 +1,11 @@
 function getMaxOrMin(type, numbers) {
     let getNum = numbers[0];
 
-    if (type !== "min" && type !== "max") throw new Error("올바른 타입이 아닙니다.");
+    if (type !== NumberCompareType.MIN && type !== NumberCompareType.MAX) throw new Error("올바른 타입이 아닙니다.");
 
     const condition = function (idx) {
-        if (type === "min") return getNum > numbers[idx];
-        if (type === "max") return getNum < numbers[idx];
+        if (type === NumberCompareType.MIN) return getNum > numbers[idx];
+        if (type === NumberCompareType.MAX) return getNum < numbers[idx];
     };
 
     for (let i = 1; i < numbers.length; i++) {
@@ -19,6 +19,11 @@ function getMaxOrMin(type, numbers) {
 
 const numbers = [0, -50, 444, 1, 23, 14, 5, 6, 234, 99];
 
-console.log(getMaxOrMin("max", numbers)) // 444
-console.log(getMaxOrMin("min", numbers)) // -50
-console.log(getMaxOrMin(111, numbers)) // "올바른 타입이 아닙니다."
+const NumberCompareType = {
+    MIN: "MIN",
+    MAX: "MAX"
+};
+
+console.log(getMaxOrMin(NumberCompareType.MAX, numbers)) // 444
+console.log(getMaxOrMin(NumberCompareType.MIN, numbers)) // -50
+console.log(getMaxOrMin(111, numbers)) // Uncaught Error: 올바른 타입이 아닙니다.
