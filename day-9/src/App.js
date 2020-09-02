46027534ec316b09f0f26c537638e88c7b247b43
tnwjd2050 <tnwjd2050@naver.com>
2020-09-02 23:23:43 +0900

    컴포넌트 분리

--- day-9/src/App.js
+++ day-9/src/App.js
@@ -1,54 +1,12 @@
 import React from "react";
-import data from "../data.js"
 import Counter from "./components/Counter/Counter";
-import RocketFreshIcon from "./components/RocketIcon/RocketFreshIcon";
-import RocketWowIcon from "./components/RocketIcon/RocketWowIcon";
 
-console.log(data)
-const products = data.products
-function type({type}) {
-  switch (type) {
-    case "ROCKET_FRESH" : return <RocketFreshIcon />;
-    case "ROCKET_WOW" : return <RocketWowIcon />;
-    default: return ""
-  }
-}
+import Table from "./components/Table/Table";
+
 export default function App() {
   return (
     <div>
-      <table>
-        <tbody>
-        {products.map(product => {
-          return (
-            <tr key={product.id}>
-              <td>
-                <input type={"checkbox"}/>
-              </td>
-              <td>
-                <img src={product.img} alt={product.name} />
-              </td>
-              <td>
-                {product.name}
-              </td>
-              <td>
-                {product.price}
-              </td>
-              <td>
-                <select defaultValue={1}>
-                  <option>1</option>
-                </select>
-              </td>
-              <td>
-                {product.price * product.count}
-              </td>
-              <td>
-                {type(product)}
-              </td>
-            </tr>
-          )
-        })}
-        </tbody>
-      </table>
+    { <Table /> }
     </div>
   )
 }
---
+++ day-9/src/components/Table/Table.js
@@ -0,0 +1,77 @@
+import React from "react";
+import data from "../../../data.js"
+import RocketFreshIcon from "../../components/RocketIcon/RocketFreshIcon";
+import RocketWowIcon from "../../components/RocketIcon/RocketWowIcon";
+
+const rocketType = {
+  ROCKET_FRESH: "ROCKET_FRESH",
+  ROCKET_WOW: "ROCKET_WOW"
+}
+
+const products = data.products
+const freshProducts = products.filter(item => item.type === rocketType.ROCKET_FRESH)
+const wowProducts = products.filter(item => item.type === rocketType.ROCKET_WOW)
+
+function type({type}) {
+  switch (type) {
+    case rocketType.ROCKET_FRESH : return <RocketFreshIcon />;
+    case rocketType.ROCKET_WOW : return <RocketWowIcon />;
+    default: return ""
+  }
+}
+
+function caption({type}) {
+  switch (type) {
+    case rocketType.ROCKET_FRESH : return "로켓프레시 상품";
+    case rocketType.ROCKET_WOW: return "로켓배송 상품(로켓와우 포함)";
+    default: return ""
+  }
+}
+
+const tabelComponent = data => {
+  console.log(data)
+  return <table>
+          <caption>{caption(data[0])}</caption>
+          <tbody>
+            {data.map(product => {
+              return (
+                <tr key={product.id}>
+                <td>
+                    <input type={"checkbox"}/>
+                </td>
+                <td>
+                    <img src={product.img} alt={product.name} />
+                </td>
+                <td>
+                    {product.name}
+                </td>
+                <td>
+                    {product.price}
+                </td>
+                <td>
+                    <select defaultValue={1}>
+                    <option>1</option>
+                    </select>
+                </td>
+                <td>
+                    {product.price * product.count}
+                </td>
+                <td>
+                    {type(product)}
+                </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+}
+
+export default function Table() {
+
+  return (
+    <div>
+        { tabelComponent(freshProducts) }
+        { tabelComponent(wowProducts) }
+    </div>
+  )
+}
