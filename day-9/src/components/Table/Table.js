bc6b59618f2eb3bf79f362162b61367ef09d48ac
tnwjd2050 <tnwjd2050@naver.com>
2020-09-02 23:31:46 +0900

    판매자 배송상품 추가

--- day-9/src/components/Table/Table.js
+++ day-9/src/components/Table/Table.js
@@ -5,12 +5,14 @@ import RocketWowIcon from "../../components/RocketIcon/RocketWowIcon";
 
 const rocketType = {
   ROCKET_FRESH: "ROCKET_FRESH",
-  ROCKET_WOW: "ROCKET_WOW"
+  ROCKET_WOW: "ROCKET_WOW",
+  PARTNER: "PARTNER"
 }
 
 const products = data.products
 const freshProducts = products.filter(item => item.type === rocketType.ROCKET_FRESH)
 const wowProducts = products.filter(item => item.type === rocketType.ROCKET_WOW)
+const partnerProducts = products.filter(item => item.type === rocketType.PARTNER)
 
 function type({type}) {
   switch (type) {
@@ -24,6 +26,7 @@ function caption({type}) {
   switch (type) {
     case rocketType.ROCKET_FRESH : return "로켓프레시 상품";
     case rocketType.ROCKET_WOW: return "로켓배송 상품(로켓와우 포함)";
+    case rocketType.PARTNER: return "판매자배송 상품";
     default: return ""
   }
 }
@@ -72,6 +75,7 @@ export default function Table() {
     <div>
         { tabelComponent(freshProducts) }
         { tabelComponent(wowProducts) }
+        { tabelComponent(partnerProducts) }
     </div>
   )
 }
