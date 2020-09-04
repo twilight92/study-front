71564ddd617b2195b7780816c3d9dc203a8699ea
tnwjd2050 <tnwjd2050@naver.com>
2020-09-04 23:02:33 +0900

    정렬정리

--- day-9/src/components/SelectBox/SelectBox.js
+++ day-9/src/components/SelectBox/SelectBox.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function SelectBox({inventory}) {
   return (
     <select defaultValue={1}>
-          {
-            [...Array(10).keys()].map((item, idx) => {
-              const index = idx + 1;
-              return <option value={index} disabled={index > inventory}>{index}</option>
-            })
-          }
-        </select>
+      {
+        [...Array(10).keys()].map((item, idx) => {
+          const index = idx + 1;
+          return <option value={index} disabled={index > inventory}>{index}</option>
+        })
+      }
+    </select>
   )
 }
